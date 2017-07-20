7b7710bbc7c6e166753d119e2538a757df848b12
Pimeko <benjamin.widaw@gmail.com>
2017-07-20 11:04:31 +0200

    change env values for connection

--- config/database.php
+++ config/database.php
@@ -81,8 +81,8 @@ return [
         'testing' => [
             'driver'    => 'mysql',
             'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'forge'),
-            'username'  => env('DB_TEST_USERNAME', 'forge'),
+            'database'  => env('DB_TEST_DATABASE', 'dabster'),
+            'username'  => env('DB_TEST_USERNAME', 'root'),
             'password'  => env('DB_TEST_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
